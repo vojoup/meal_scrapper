c60894c4aade0bd4c53ec7c84a03e77ff2a59973
Vojta Oupicky <voj.oup@gmail.com>
2017-09-29 15:41:47 +0200

    More meal sections
    More meal sections were added. Now outputs main courses and daily offer as well.

--- scraper.py
+++ scraper.py
@@ -9,9 +9,15 @@ uClient.close()
 page_soup = soup(page_html, "html.parser")
 # Grabs each section with class "list-new"
 main_courses = page_soup.findAll("section", {"class":"list-new"})
+daily_offers = page_soup.findAll("section", {"class":"red-section"})
 # Selects just main course from those sections
 course = main_courses[0]
+daily_offer = daily_offers[1]
 print('Hlavni jidla:')
 # Loops through each meal and prints it on sepate lines
 for course in course.findAll("h3"):
     print(course.text)
+print('')
+print('Denni nabidka:')
+for meal in daily_offer.findAll("h3"):
+    print(meal.text)
